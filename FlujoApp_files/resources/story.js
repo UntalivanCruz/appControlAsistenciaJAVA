7a2da56d08e7fe51823558b357c5130e11d0e852
Ivan Cruz <ivancruhn@gmail.com>
2022-08-22 22:18:33 -0600

    Programacion login

---
+++ FlujoApp_files/resources/story.js
@@ -0,0 +1,113 @@
+var story = {
+ "pages": [
+  {
+   "image": "login.png",
+   "image2x": "login@2x.png",
+   "width": 553,
+   "links": [{
+    "rect": [
+     192,
+     256,
+     254,
+     281
+    ],
+    "page": 1
+   }],
+   "title": "login",
+   "height": 377
+  },
+  {
+   "image": "menu.png",
+   "image2x": "menu@2x.png",
+   "width": 617,
+   "links": [
+    {
+     "rect": [
+      185,
+      19,
+      267,
+      46
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      0,
+      66,
+      80,
+      87
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      0,
+      87,
+      80,
+      108
+     ],
+     "page": 0
+    }
+   ],
+   "title": "menu",
+   "height": 417
+  },
+  {
+   "image": "asistencia.png",
+   "image2x": "asistencia@2x.png",
+   "width": 633,
+   "links": [
+    {
+     "rect": [
+      200,
+      365,
+      266,
+      390
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      344,
+      365,
+      410,
+      390
+     ],
+     "page": 1
+    }
+   ],
+   "title": "asistencia",
+   "height": 449
+  },
+  {
+   "image": "confirmarAsistencia.png",
+   "image2x": "confirmarAsistencia@2x.png",
+   "width": 467,
+   "links": [
+    {
+     "rect": [
+      277,
+      93,
+      357,
+      117
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      372,
+      93,
+      452,
+      117
+     ],
+     "page": 2
+    }
+   ],
+   "title": "confirmarAsistencia",
+   "height": 132
+  }
+ ],
+ "resolutions": [2],
+ "title": "FlujoApp",
+ "highlightLinks": false
+}
